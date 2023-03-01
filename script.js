1c29314fab5fee2ed26b42a21864a82969e0294e
Alisson Evangelista <108416106+AlissonED@users.noreply.github.com>
2023-03-01 10:44:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function resp1() {
+
+    var resp1 = "Fico feliz em te conhecer "
+
+    var nome = document.getElementById("nome").value;
+
+    document.getElementById("resp1").innerHTML = resp1 + nome;
+
+}
+
+function resp2() {
+
+    var resp2 = "Que interessante você querer "
+
+    var vida = document.getElementById("vida").value;
+
+    document.getElementById("resp2").innerHTML = resp2 + vida;
+
+}
+
+function resp3() {
+
+    var resp3 = "Entendo.. Mas seja o que acontecer, espero que dê tudo certo a você! "
+
+    document.getElementById("resp3").innerHTML = resp3;
+
+}
